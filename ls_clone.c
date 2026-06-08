c3d8272839ef37642eee00c5c672d459c49d499e
yigdrassil <cristian.timofi@gmail.com>
2026-06-08 16:46:43 +0300

    Well it is not working, probably will mess with this tomorrow

--- ls_clone.c
+++ ls_clone.c
@@ -10,8 +10,115 @@
 
 #include <unistd.h> // Need this for the getopt() function to process cmd line arguments
 
+#include <sys/stat.h> // This one contains the struct for the different data on the files/folders
+
+
+/**
+ * types.h is needed by the pwd.h library. What we need is the pwd.h library to use the lookup functions to actually make the numbers into user id and group
+ * 
+ * for more information check out the man 3 getpwuid
+ */
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+
 
 int show_all = 0; //declaring an option for the ls command
+
+
+/**
+ * Hleper func for gettint the appropriate string formatting for each file/document
+ * 
+ * check man 2 stat -> talks about the C stat library and some implementations
+ * 
+ * check man 7 inode -> talks about the macros implemented to check if the file is a dir, a socket, a block device, etc, or the masks if i wanna go full nelson :))
+ */
+
+void mode_string(mode_t mode, char *str)
+{
+    //fingering out the file/dir type, well dir is also a file, but hey
+    if (S_ISDIR(mode))
+    {
+        str[0] = 'd';
+    }
+    else if (S_ISLNK(mode))
+    {
+        str[0] = 'l';
+    }
+    else if (S_ISCHR(mode))
+    {
+        str[0] = 'c';
+    }
+    else if (S_ISBLK(mode))
+    {
+        str[0] = 'b';
+    }
+    else if (S_ISFIFO(mode))
+    {
+        str[0] = 'p';
+    }
+    else if (S_ISSOCK(mode))
+    {
+        str[0] = 's';
+    }
+    else
+    {
+        str[0] = '-';
+    }
+
+
+    // Do the checks for the rights part, with masks
+    str[1] = (mode & S_IRUSR) ? 'r' : '-';
+    str[2] = (mode & S_IWUSR) ? 'w' : '-';
+    str[3] = (mode & S_IXUSR) ? 'x' : '-';
+    str[4] = (mode & S_IRGRP) ? 'r' : '-';
+    str[5] = (mode & S_IWGRP) ? 'w' : '-';
+    str[6] = (mode & S_IXGRP) ? 'x' : '-';
+    str[7] = (mode & S_IROTH) ? 'r' : '-';
+    str[8] = (mode & S_IWOTH) ? 'w' : '-';
+    str[9] = (mode & S_IXOTH) ? 'x' : '-';
+    str[10] = '\0'; //Terminathor char
+
+}
+
+
+
+/**
+ * The print function fo print user names and the group
+ */
+
+void print_long(const char *dir, const char *name)
+{
+    char fullpath[4096]; //This is the full path length on linux, would take a jab at windows but i am too tired at this point :) maybe next time
+
+    snprintf(fullpath, sizeof(fullpath), "%s%s", dir, name);
+
+    struct stat st;
+
+    if ( lstat(fullpath, &st) < 0 ) //fill up the empty stats struct with info, if there is nothing, then we use perror to print error message, and we exit
+    {
+        perror(name);
+
+        return; //Cause da func is void
+    }
+
+    char modes[11];
+
+mode_string(st.st_mode, modes);
+
+    struct passwd *pw = getpwuid(st.st_uid);
+    struct group *gr = getgrgid(st.st_gid);
+
+    const char *user = pw ? pw->pw_name : "?";
+    const char *group = gr ? gr->gr_name : "?";
+
+    printf("%s %s %s %s \n", modes, user, group, name);
+
+
+
+}
+
+
 
 int main(int argc, char **argv)
 {
@@ -74,9 +181,10 @@ int main(int argc, char **argv)
         {
             continue; 
         } 
-            
+        
+        print_long(path, entry->d_name);
 
-        printf( "%s \n", (*entry).d_name );  // entry->d_name can be rewritten as (*entry).d_name  you basically dereference the pointer
+        //printf( "%s \n", (*entry).d_name );  // entry->d_name can be rewritten as (*entry).d_name  you basically dereference the pointer
     }
 
     closedir(dirpath);
