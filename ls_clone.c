ab70b0b11979ab4fed007eeb6bdf2f7b44b7254e
yigdrassil <cristian.timofi@gmail.com>
2026-06-08 14:28:41 +0300

    committing all ze stuff, this is an initial commit, to keep track of changes, in case i want to see what i did later

---
+++ ls_clone.c
@@ -0,0 +1,55 @@
+/** Trying to make a smaller implementation of the ls command, to practice C a little bit */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <dirent.h> // This library contains the DIR pointer for a directory and functions to manipulate the DIR pointer. Check man page for it
+
+#include <errno.h> // Using this to handle possible errors
+
+
+int main(int argc, char **argv)
+{
+
+    const char *path = argv[1];
+
+    DIR *dirpath = opendir(path);
+
+    if(!dirpath)
+    {
+        perror("opendir");
+
+        return 1;
+    }
+
+    struct dirent *entry; // Declaring a struct of type dirent, to access the value returned by readdir() function
+    
+
+/** If the function is continuously called, it reads the next directory in the dirpath. Make it a loop and iterate untill NULL !!!Need to debug this shit to understand it!!!
+ * 
+    entry = readdir(dirpath); // Storing the info from readdir func
+    printf("%s\n", entry->d_name);
+
+    entry = readdir(dirpath); // Storing the info from readdir func
+    printf("%s\n", entry->d_name);
+
+    entry = readdir(dirpath); // Storing the info from readdir func
+    printf("%s\n", entry->d_name);
+
+    entry = readdir(dirpath); // Storing the info from readdir func
+    printf("%s\n", entry->d_name);
+
+    entry = readdir(dirpath); // Storing the info from readdir func
+    printf("%s\n", entry->d_name);
+*/
+
+    while ( (entry = readdir(dirpath)) != NULL )
+    {
+        printf( "%s \n", (*entry).d_name );  // entry->d_name can be rewritten as (*entry).d_name  you basically dereference the pointer
+    }
+
+asdfasdfasdf
+    return 0;
+
+}
